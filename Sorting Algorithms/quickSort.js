fb8f3f1700fb582eaa6ba77c373aa5968823e151
djebby <djebby.firas@gmail.com>
2021-11-04 19:32:42 +0100

    adding the quickSort algorithm

---
+++ Sorting Algorithms/quickSort.js
@@ -0,0 +1,12 @@
+const quickSort = (unsortedArray) => {
+    if(unsortedArray.length <= 1) return unsortedArray;
+
+    const pivot = unsortedArray[unsortedArray.length-1];
+    let leftArray = [];
+    let rightArray = [];
+
+    for(let element of unsortedArray.slice(0, unsortedArray.length-1))
+        element > pivot ? rightArray.push(element) : leftArray.push(element);
+
+    return [...quickSort(leftArray), pivot, ...quickSort(rightArray)];
+}
