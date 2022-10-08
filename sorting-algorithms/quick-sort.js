817835431549ba4f49fae34a404aa7a64c27a7d1
djebby <djebby.firas@gmail.com>
2022-10-08 07:53:39 +0100

    optimization to the quick-sort...

---
+++ sorting-algorithms/quick-sort.js
@@ -0,0 +1,17 @@
+const quickSort = (array) => {
+  if (array.length <= 1) return array;
+  // this second base case will save us a lot of recursive call...
+  else if (array.length === 2)
+    return array[0] < array[1] ? array : [array[1], array[0]];
+
+  const pivot = array[0];
+  let lowerArray = [];
+  let upperArray = [];
+
+  for (const element of array.slice(1))
+    element > pivot ? upperArray.push(element) : lowerArray.push(element);
+
+  return [...quickSort(lowerArray), pivot, ...quickSort(upperArray)];
+};
+
+//https://www.linkedin.com/in/djebby-firas/
