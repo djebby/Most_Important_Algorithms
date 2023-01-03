8370ae8349eb0e1864e39e88634dbced81d77aa3
djebby <djebby.firas@gmail.com>
2023-01-03 21:07:25 +0100

    [leetcode] solving first problem (1)

---
+++ leetcode/1-two-sum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+# Time: O(n), Space: O(n)
+def twoSum(nums: List[int], target: int) -> List[int]:
+    nums_index = {}
+
+    for i, n in enumerate(nums):
+        complement = target - n
+        if complement in nums_index:
+            return [nums_index[complement], i]
+        nums_index[n] = i
