be04d5b3975fc121fbdcf82ed123f5be7b012058
djebby <djebby.firas@gmail.com>
2023-01-24 19:59:50 +0100

    [leetcode] solving problem roman to integer

---
+++ leetcode/13-roman-to-integer.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/roman-to-integer/ (top-interview-questions)
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbolValue = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+        integer = 0
+
+        for i in range(len(s)):
+            if i < len(s)-1 and symbolValue[s[i]] < symbolValue[s[i+1]]:
+                integer -= symbolValue[s[i]]
+            else: 
+                integer += symbolValue[s[i]]
+        return integer
