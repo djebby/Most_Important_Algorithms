c4071fb1bf3d016a70c9f1cb491b930095c93652
djebby <djebby.firas@gmail.com>
2023-03-07 14:37:21 +0100

    [leetcode] solving problem n°2 add two numbers

--- leetcode/169-majority-element.py
+++ leetcode/169-majority-element.py
@@ -3,20 +3,17 @@
 from typing import List
 
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int: # Time: O(n), Space: O(1)
-        currentMajorityElement, count = None, 0
-
-        for num in nums:
-            if count == 0: currentMajorityElement = num
-            if num == currentMajorityElement: count += 1
-            else: count -= 1
-
-        return currentMajorityElement
+	def majorityElement(self, nums: List[int]) -> int: # Time: O(n), Space: O(1)
+		element, count = None, 0
+		for num in nums:
+			if count == 0: element = num
+			count += 1 if element == num else -1
+		return element
     
 
-    def majority_element(self, nums: List[int]) -> int: # Time: O(n), Space: O(n)
-        occurances = dict()
-        for num in nums:
-            occurances[num] = 1 + occurances.get(num, 0)
-            if occurances[num] > (len(nums) / 2): return num
+	def majority_element(self, nums: List[int]) -> int: # Time: O(n), Space: O(n)
+		occurances = dict()
+		for num in nums:
+			occurances[num] = 1 + occurances.get(num, 0)
+			if occurances[num] > (len(nums) / 2): return num
 
--- leetcode/20-valid-parentheses.py
+++ leetcode/20-valid-parentheses.py
@@ -1,13 +1,25 @@
 # https://leetcode.com/problems/valid-parentheses/ (top-interview-questions)
 
 class Solution:
-    def isValid(self, s: str) -> bool:
-        closer = {')': '(', ']': '[', '}': '{'}
-        validation_stack = []
-        for c in s:
-            if c in closer:
-                if validation_stack and closer[c] == validation_stack[-1]:
-                    validation_stack.pop()
-                else: return False
-            else: validation_stack.append(c)
-        return False if validation_stack else True
+	def isValid(self, s: str) -> bool:
+		closing = {')': '(', ']': '[', '}': '{'}
+		opening = list()
+		for c in s:
+			if c in closing:
+				if len(opening) > 0 and closing[c] == opening[-1]:
+					opening.pop()
+				else: return False
+			else: opening.append(c)
+
+		return len(opening) == 0
+	
+
+	def is_valid(self, s: str) -> bool:
+		opening = {'(': ')', '{': '}', '[': ']'}
+		stack = list()
+		for char in s:
+			if char in opening:
+				stack.append(char)
+			elif not stack or opening[stack.pop()] != char:
+				return False
+		return len(stack) == 0
