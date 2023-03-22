c8f7a7a322df8f88e7f3ba8ba23b804ae669b11c
djebby <djebby.firas@gmail.com>
2023-03-22 18:31:34 +0100

    [leetcode] solving problem n°64 minimum path sum

---
+++ leetcode/MinimumPathSum64.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class MinimumPathSum64 {
+	
+	public int minPathSum(int[][] grid) {
+		int ROWS = grid.length;
+		int COLS = grid[0].length;
+		int[] dp = new int[COLS];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[COLS-1] = grid[ROWS-1][COLS-1];
+
+		for(int row = ROWS-1; row >= 0; row--) {
+			for(int col = COLS-1; col >= 0; col--) {
+				if(row == ROWS - 1 && col == COLS - 1)
+					continue;
+				int right =  col+1 < COLS ? dp[col+1] : Integer.MAX_VALUE;
+				int down = dp[col];
+				dp[col] = grid[row][col] + Math.min(right, down);
+			}
+		}
+
+		return dp[0];
+	}
+}
