76e170e2533062d79783a84b5950f6bf394f9e8f
djebby <djebby.firas@gmail.com>
2023-08-20 08:06:35 +0100

    [leetcode][javascript] problem n°88 merge sorted array

--- leetcode/88-merge-sorted-array.py
+++ leetcode/88-merge-sorted-array.py
@@ -3,36 +3,35 @@
 from typing import List
 
 class Solution:
-	def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-		"""
-		Do not return anything, modify nums1 in-place instead.
-		"""
-		last = m + n - 1
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        last = m + n - 1
 
-		while m > 0 and n > 0:
-			if nums1[m-1] > nums2[n-1]:
-				nums1[last] = nums1[m - 1]
-				m -= 1
-			else:
-				nums1[last] = nums2[n-1]
-				n -= 1
-			last -= 1
-		while n > 0:
-			nums1[last] = nums2[n-1]
-			n -= 1
-			last -= 1
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[last] = nums2[n-1]
+                n -= 1
+            last -= 1
+        while n > 0:
+            nums1[last] = nums2[n-1]
+            n -= 1
+            last -= 1
 
 
-	def merge0(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-		"""
-		Do not return anything, modify nums1 in-place instead.
-		"""
-		for i in range(n): nums1.pop()
+    def merge0(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(n): nums1.pop()
 
-		for num in nums2:
-			i = 0
-			nums1.insert(0, num)
-			while i < len(nums1)-1 and nums1[i] > nums1[i+1]:
-				nums1[i], nums1[i+1] = nums1[i+1], nums1[i]
-				i += 1
-
+        for num in nums2:
+            i = 0
+            nums1.insert(0, num)
+            while i < len(nums1)-1 and nums1[i] > nums1[i+1]:
+                nums1[i], nums1[i+1] = nums1[i+1], nums1[i]
+                i += 1
