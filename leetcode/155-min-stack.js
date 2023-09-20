99b420b2694cb6d729c9949c90815f9cb79629d1
djebby <djebby.firas@gmail.com>
2023-09-20 06:43:56 +0100

    [leetcode][js] problem n°155 min stack

---
+++ leetcode/155-min-stack.js
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/min-stack/ (top-interview-questions)
+
+var MinStack = function() {
+  this.stack = [];
+  this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+  this.stack.push(val);
+  this.minStack.push((this.minStack.length === 0) ? val : Math.min(val, this.minStack[this.minStack.length - 1]));
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+  this.stack.pop();
+  this.minStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+  return this.minStack[this.minStack.length - 1];
+};
