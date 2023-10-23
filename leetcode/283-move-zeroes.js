a375d78f41966d59b47e79431d88c67ee71907a1
djebby <djebby.firas@gmail.com>
2023-10-23 20:21:52 +0100

    [leetcode][js] problem n°283 move zeroes

---
+++ leetcode/283-move-zeroes.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/move-zeroes/ (top-interview-questions)
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] !== 0) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      j += 1;
+    }
+  }
+};
--- leetcode/283-move-zeroes.py
+++ leetcode/283-move-zeroes.py
@@ -2,7 +2,6 @@
 
 from typing import List
 
-
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
