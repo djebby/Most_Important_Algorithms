03c945bd02a4861d9bac0e9ccc651795df800d64
djebby <djebby.firas@gmail.com>
2024-01-02 10:57:45 +0100

    [leetcode-daily] change the solution of problem n°2610 convert an array into a 2d array with conditions

--- leetcode/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ leetcode/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -7,18 +7,13 @@ class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
         nums_count = Counter(nums)
         
-        max_num_count = 0
-        
-        for _, val in nums_count.items():
-            max_num_count = max(max_num_count, val)
-        
-        two_dim_arr = [[] for _ in range(max_num_count)]
+        two_dim_arr = [[] for _ in range(max(nums_count.values()))]
 
         for num in nums_count:
-            r = 0
+            row = 0
             while nums_count[num] > 0:
-                two_dim_arr[r].append(int(num))
+                two_dim_arr[row].append(int(num))
                 nums_count[num] -= 1
-                r += 1
+                row += 1
 
         return two_dim_arr
